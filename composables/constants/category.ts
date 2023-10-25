f4f4220a60d2511f6a1d3c024113ce7518cdf718
onyourza <zanikurnia86@gmail.com>
2023-10-25 16:16:27 +0700

    first commit

---
+++ composables/constants/category.ts
@@ -0,0 +1,38 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Jam Tangan",
+    },
+    {
+    id: 2,
+    name: "Tas",
+    },
+    {
+    id: 3,
+    name: "Sepatu",
+    },
+    {
+    id: 4,
+    name: "Buku Tulis",
+    },
+    {
+    id: 5,
+    name: "Pensil",
+    },
+    {
+    id: 6,
+    name: "Tempat Pensil",
+    },
+    {
+    id: 7,
+    name: "Seragam",
+    },
+    {
+    id: 8,
+    name: "Kaus Kaki",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
